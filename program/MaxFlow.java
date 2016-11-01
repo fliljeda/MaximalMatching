89d7392b47ccaab69a505cac4517f32f623dcfb8
Peonsson <xsnudelx@hotmail.com>
2016-11-01 15:24:36 +0100

    added printing test case

--- program/MaxFlow.java
+++ program/MaxFlow.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MaxFlow {
 
-    public int numberOfVertices;
-    public int numberOfEdges;
-    public int source;
-    public int sink;
-    public Kattio io;
+    private int numberOfVertices;
+    private int numberOfEdges;
+    private int source;
+    private int sink;
+    private Kattio io;
+
+    private int[][] capacity = new int[2000][2000];
+    private int[][] restCapacity = new int[2000][2000];
+    private int[][] flow = new int[2000][2000];
+    private HashMap<Integer, ArrayList<Integer>> adjacencyList;
 
     private class Node {
         public Node previous;
@@ -20,9 +28,33 @@ public class MaxFlow {
         source = io.getInt();
         sink = io.getInt();
         numberOfEdges = io.getInt();
+        adjacencyList = new HashMap<Integer, ArrayList<Integer>>(5000);
 
         for (int i = 0; i < numberOfEdges; i++) {
+            int src = io.getInt();
+            int dest = io.getInt();
+            int cap = io.getInt();
+            capacity[src][dest] = cap;
+            restCapacity[src][dest] = cap;
+            ArrayList val = adjacencyList.get(src);
+            if(val == null) {
+                adjacencyList.put(src, new ArrayList<Integer>(10));
+                val = adjacencyList.get(src);
+            }
+            val.add(dest);
+        }
+        printAdjacencyList(adjacencyList);
+    }
 
+    public void printAdjacencyList(HashMap<Integer, ArrayList<Integer>> list){
+        for(int i: list.keySet()){
+            System.out.print("Key: " + i);
+            ArrayList<Integer> al = list.get(i);
+            System.out.println(" Values:");
+            for(int val : al){
+                System.out.print(" " + val);
+            }
+            System.out.println();
         }
     }
 
