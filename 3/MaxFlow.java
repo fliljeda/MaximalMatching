123538ee4c5627edb15130323a4ac6d0eaac2321
Fredrik Liljedahl <fredrik.liljedahl@hotmail.com>
2016-11-04 12:26:18 +0100

    Final

--- 3/MaxFlow.java
+++ 3/MaxFlow.java
@@ -8,10 +8,19 @@ public class MaxFlow {
     private int sink;
     private int flowEdges;
 
-    private int[][] capacity = new int[5001][5001];
-    private int[][] flow = new int[5001][5001];
-    private HashMap<Integer, ArrayList<Integer>> adjacencyList = 
-        new HashMap<Integer, ArrayList<Integer>>(7000);
+    private HashMap<Integer, HashMap<Integer,Edge>> adjacencyList = 
+        new HashMap<Integer, HashMap<Integer,Edge>>(7000);
+    
+    private class Edge{
+        public int id = 0;
+        public int cap = 0;
+        public int flow = 0;
+        public Edge(int id, int cap, int flow){
+            this.id = id;
+            this.cap = cap;
+            this.flow = flow;
+        }
+    }
 
     public void initGraph(int vertices, int source, int sink, int edges) {
         numberOfVertices = vertices;
@@ -25,31 +34,31 @@ public class MaxFlow {
     }
 
     public void readEdge(int src, int dest, int cap) {
-        capacity[src][dest] = cap;
-        ArrayList<Integer> val = adjacencyList.get(src);
+        HashMap<Integer, Edge> val = adjacencyList.get(src);
         if (val == null) {
-            adjacencyList.put(src, new ArrayList<Integer>(50));
+            adjacencyList.put(src, new HashMap<Integer,Edge>(10));
             val = adjacencyList.get(src);
         }
-        val.add(dest);
+        val.put(dest, new Edge(dest,cap,0));
 
         // FREDRIK: Added opposite edges also
         if (src != source && dest != sink) {
-            ArrayList<Integer> val2 = adjacencyList.get(dest);
+            HashMap<Integer,Edge> val2 = adjacencyList.get(dest);
             if (val2 == null) {
-                adjacencyList.put(dest, new ArrayList<Integer>(50));
+                adjacencyList.put(dest, new HashMap<Integer,Edge>(10));
                 val2 = adjacencyList.get(dest);
             }
-            val2.add(src);
+            val2.put(src, new Edge(src,0,0));
         }
     }
     
     public void printlist(){
         for(int i : adjacencyList.keySet()){
-            ArrayList<Integer> al = adjacencyList.get(i);
-            for(int j : al){
-                System.out.println(i + "->" + j);
+            HashMap<Integer,Edge> al = adjacencyList.get(i);
+            for(int j : al.keySet()){
+                System.out.println(i + "->" + j + " |c: " + al.get(j).cap);
             }
+            System.out.println("-------------");
         }
     }
 
@@ -62,15 +71,15 @@ public class MaxFlow {
 
     public ArrayList<String> getSolution() {
         ArrayList<String> res = new ArrayList<String>(numberOfEdges);
-        for (int i = 0; i < flow.length; i++) {
-            for (int j = 0; j < flow[i].length; j++) {
+        for (int i: adjacencyList.keySet()) {
+            for (int j: adjacencyList.get(i).keySet()) {
 
                 if (i == source || j == source)
                     continue;
                 if (i == sink || j == sink)
                     continue;
 
-                if (flow[i][j] > 0) {
+                if (getFlow(i,j) > 0) {
                     flowEdges++;
                     res.add((i - 1) + " " + (j - 1));
                 }
@@ -78,13 +87,34 @@ public class MaxFlow {
         }
         return res;
     }
+    
+    /*Gets capacity of edge*/
+    public int getCap(int src, int dest){
+        return adjacencyList.get(src).get(dest).cap;
+        
+    }
+    /*Gets flow of edge*/
+    public int getFlow(int src, int dest){
+        return adjacencyList.get(src).get(dest).flow;
+    }
+    
+    /*Adds capacity to the given edge*/
+    public void addCap(int src, int dest, int add){
+        if(src == sink || dest == source)return;
+        adjacencyList.get(src).get(dest).cap += add;
+    }
+    /*Adds flow to the given edge*/
+    public void addFlow(int src, int dest, int add){
+        if(src == sink || dest == source)return;
+        adjacencyList.get(src).get(dest).flow += add;
+    }
 
     private boolean BFS() {
         if (adjacencyList.get(source) == null) {
             return false;
         }
         int[] parent = new int[numberOfVertices + 1];
-        Arrays.fill(parent, -1); //FREDRIK:efficiency use 0 (we go from 1 to n)
+        //Arrays.fill(parent, -1); //FREDRIK:efficiency use 0 (we go from 1 to n)
         parent[source] = source;
         int[] m = new int[numberOfVertices + 1]; // Capacity of path to node
         m[source] = Integer.MAX_VALUE;
@@ -93,23 +123,23 @@ public class MaxFlow {
         queue.offer(source);
         while (!queue.isEmpty()) {
             int u = queue.poll();
-            ArrayList<Integer> neighbors = adjacencyList.get(u);
+            HashMap<Integer,Edge> neighbors = adjacencyList.get(u);
             if (neighbors == null) {
                 continue;
             }
-            for (int v : neighbors) {
+            for (int v : neighbors.keySet()) {
                 // There is available capacity and v haven't been visited before
-                if (capacity[u][v] > flow[u][v] && parent[v] == -1) {
+                if (getCap(u,v) > getFlow(u,v) && parent[v] == 0) {
                     parent[v] = u;
-                    m[v] = Math.min(m[u], capacity[u][v] - flow[u][v]);
+                    m[v] = Math.min(m[u], getCap(u,v) - getFlow(u,v));
                     if (v != sink)
                         queue.offer(v);
                     else {
                         // Backtrack search and write flow
                         while (parent[v] != v) {
                             u = parent[v];
-                            flow[u][v] += m[sink];
-                            flow[v][u] -= m[sink];
+                            addFlow(u,v,m[sink]);
+                            addFlow(v,u,-m[sink]);
                             v = u;
                         }
                         return true;
